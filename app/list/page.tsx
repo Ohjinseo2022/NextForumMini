2c7a2aa386f74135cdacfb2ddf54f9e16118eece
Ohjinseo2022 <dhwlstj@gmail.com>
2023-06-03 16:31:53 +0900

    게시판 미니프로젝트시작

---
+++ app/list/page.tsx
@@ -0,0 +1,14 @@
+export default list = () => {
+  return (
+    <div className="list-bg">
+      <div className="list-item">
+        <h4>글제목</h4>
+        <p>1월1일</p>
+      </div>
+      <div className="list-item">
+        <h4>글제목</h4>
+        <p>1월1일</p>
+      </div>
+    </div>
+  )
+}
